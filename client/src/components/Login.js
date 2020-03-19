1a6e9fc7ca50ed0882def68fbadb3effbbb7ae4f
Ignacio <shoik.xtazy@gmail.com>
2020-03-19 20:32:11 -0300

    login redux step 2

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,13 +5,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Alert from '@material-ui/lab/Alert';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from './../actions/authActions';
+import { clearErrors } from './../actions/errorActions';
 
 
 const useStyles = makeStyles(theme => ({
@@ -35,15 +39,122 @@ const useStyles = makeStyles(theme => ({
 }));
 
 class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+        msg: null
+    };
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        error: PropTypes.object.isRequired,
+        login: PropTypes.func.isRequired,
+        clearErrors: PropTypes.func.isRequired
+    };
+
+    componentDidUpdate(prevProps) {
+        const { error, isAuthenticated } = this.props;
+        if (error !== prevProps.error) {
+            // Check for register error
+            if (error.id === 'LOGIN_FAIL') {
+                this.setState({ msg: error.msg.msg });
+            } else {
+                this.setState({ msg: null });
+            }
+        }
+
+        // If authenticated, close modal
+        if (this.state.modal) {
+            if (isAuthenticated) {
+                this.toggle();
+            }
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const { email, password } = this.state;
+
+        const user = {
+            email,
+            password
+        };
+
+        // Attempt to login
+        this.props.login(user);
+    };
 
     render() {
-        const { classes } = this.props;
+        const classes = useStyles;
         return (
-            <div>
-                <h1>Hola mundo</h1>
-            </div>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper} style={{ marginTop: '8rem' }}>
+
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form} onSubmit={this.onSubmit}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            onChange={this.onChange}
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            onChange={this.onChange}
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                         </Button>
+
+                        {this.state.msg ? (
+                            <Alert severity="error">{this.state.msg}</Alert>
+                        ) : null}
+
+                    </form>
+                </div>
+            </Container>
         );
     }
 }
 
-export default Login;
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error
+});
+
+export default connect(
+    mapStateToProps,
+    { login, clearErrors }
+)(Login);
